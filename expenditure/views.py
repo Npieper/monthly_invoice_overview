e4d8d0e7b210bc97de21e05a856171c2d505f296
Niklas Pieper <pieper.niklas.gm@gmail.com>
2019-03-23 16:27:45 +0100

    add base template

--- expenditure/views.py
+++ expenditure/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+expenditures = [
+    {
+        'netflix': 10.99,
+        'amazon': 7.99,
+        'xtra_fit': 15
+    }
+]
+
 
 def home(request):
-    return render(request, 'expenditure/home.html')
+    context = {
+        'expenditures': expenditures
+    }
+    return render(request, 'expenditure/home.html', context)
+
 
 def add(request):
     return HttpResponse("<h1> Adding Expenditure </h1>")
