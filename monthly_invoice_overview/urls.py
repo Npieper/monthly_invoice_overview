4692bd6881f517d54e87cd68d6a9b1ad2a7039d8
Niklas Pieper <niklas.pieper@trustedshops.com>
2019-03-25 19:49:25 +0100

    Add Form for registration

--- monthly_invoice_overview/urls.py
+++ monthly_invoice_overview/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from users import views as user_views
 
 urlpatterns = [
     path('expenditure/', include('expenditure.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register')
 ]
