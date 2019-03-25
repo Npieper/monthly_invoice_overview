5ec39948832eddd036ade18b0b1fd8367e8836ae
Niklas Pieper <niklas.pieper@trustedshops.com>
2019-03-25 19:31:20 +0100

    Database migration

--- expenditure/models.py
+++ expenditure/models.py
@@ -5,3 +5,6 @@ class Expenditure(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	name = models.CharField(max_length=100)
 	price = models.FloatField(null=True, blank=True, default=None)
+
+	def __str__(self):
+		return self.name
--- expenditure/views.py
+++ expenditure/views.py
@@ -1,35 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-expenditures = [
-    {
-        'name': 'Netflix',
-        'price': 10.99
-    },
-    {
-        'name': 'Amazon',
-        'price': 7.99
-    },
-    {
-        'name': 'Xtra-Fit',
-        'price': 15.00
-    }
-
-]
+from .models import Expenditure
 
 
 def home(request):
 
-    monthly_payment = 0.0
+    expenditures = Expenditure.objects.all()
+    monthly_payment = calculate_monthly_payment(expenditures)
 
-    for i, price in enumerate(d['price'] for d in expenditures):
-        monthly_payment += price
-
-    monthly_payment = '%.3f'%(monthly_payment)
     context = {
         'expenditures': expenditures,
         'monthly_payment': monthly_payment
     }
+
     return render(request, 'expenditure/home.html', context)
 
+def calculate_monthly_payment(expenditures):
 
+    monthly_payment = 0.0
+
+    for expenditure in expenditures:
+        monthly_payment += expenditure.price
+
+    monthly_payment = '%.3f'%(monthly_payment)
+
+    return monthly_payment
